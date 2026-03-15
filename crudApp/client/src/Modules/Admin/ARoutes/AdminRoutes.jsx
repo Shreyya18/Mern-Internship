a627358ce56f2d7b42047297f5c0da8684c2814e
shreyya18 <shreyapoojary251@gmail.com>
2026-03-15 13:01:45 +0530

    feat(client): add ViewUser route to AdminRoutes component

--- crudApp/client/src/Modules/Admin/ARoutes/AdminRoutes.jsx
+++ crudApp/client/src/Modules/Admin/ARoutes/AdminRoutes.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import {Route, Routes} from 'react-router-dom'
 import AHome from '../AComponents/AHome'
 import Sidebar from '../AComponents/Sidebar'
+import ViewUser from '../AComponents/ViewUser'
 const AdminRoutes = () => {
   return (
     <div>
       <Sidebar/>
       <Routes>
         <Route path='/AHome' element={<AHome/>} />
+        <Route path='/ViewUser' element={<ViewUser
+ViewUser/>} />
       </Routes>
     </div>
   )
