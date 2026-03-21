74644c0502c2b58ce9fd8315f739a502b6f4ca75
shreyya18 <shreyapoojary251@gmail.com>
2026-03-21 15:51:22 +0530

    product route

--- crudApp/server/Routes/product_routes.js
+++ crudApp/server/Routes/product_routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const {addproduct, getproduct, getproductbyid} = require('../Controller/product_controller')
+const {addproduct, getproduct, getproductbyid, deleteproduct, updateproduct} = require('../Controller/product_controller')
 
 const route = express.Router();
 
 route.post('/addproduct', addproduct);
 route.get('/getproduct', getproduct)
 route.get('/getproductbyid/:id', getproductbyid)
+route.delete('/deleteproduct/:id', deleteproduct)
+route.put('/updateproduct/:id', updateproduct)
 module.exports = route;
