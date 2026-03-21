831a224b5f875826c6f48ee665b05dee49b1a8d5
shreyya18 <shreyapoojary251@gmail.com>
2026-03-21 15:50:58 +0530

    view user

--- crudApp/client/src/Modules/Admin/AComponents/ViewUser.jsx
+++ crudApp/client/src/Modules/Admin/AComponents/ViewUser.jsx
@@ -8,6 +8,7 @@ import Paper from '@mui/material/Paper';
 import { border } from '@mui/system';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import Button from '@mui/material/Button';
 
 
 
@@ -25,6 +26,16 @@ export default function ViewUser() {
       console.log(error);
     })
   })
+  const HandleDelete=(uid)=>{
+    axios.delete(`http://localhost:7000/user/deleteuser/${uid}`)
+    .then((res)=>{
+        console.log(res)
+        alert("User Deleted")
+    })
+    .catch((error)=>{
+      console.log(error)
+    })
+  }
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
@@ -35,6 +46,7 @@ export default function ViewUser() {
             <TableCell align="right">EMAIL</TableCell>
             <TableCell align="right">PHONE</TableCell>
             <TableCell align="right">ADDRESS</TableCell>
+            <TableCell align='right'>ACTION</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -50,6 +62,10 @@ export default function ViewUser() {
               <TableCell align="right">{row.email}</TableCell>
               <TableCell align="right">{row.phone}</TableCell>
               <TableCell align="right">{row.address}</TableCell>
+              <TableCell align='right'>
+                  <Button variant='outlined'>UPDATE</Button>
+                  <Button variant='outlined' onClick={()=>HandleDelete(row._id)} >DELETE</Button>
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
