50fff30da3b906e8bb154c0d63d6aac72019f21a
shreyya18 <shreyapoojary251@gmail.com>
2026-03-22 12:49:26 +0530

    sidebar fix

--- crudApp/client/src/Modules/Admin/AComponents/Sidebar.jsx
+++ crudApp/client/src/Modules/Admin/AComponents/Sidebar.jsx
@@ -1,3 +1,170 @@
+// import * as React from 'react';
+// import { styled, useTheme } from '@mui/material/styles';
+// import Box from '@mui/material/Box';
+// import Drawer from '@mui/material/Drawer';
+// import CssBaseline from '@mui/material/CssBaseline';
+// import MuiAppBar from '@mui/material/AppBar';
+// import Toolbar from '@mui/material/Toolbar';
+// import List from '@mui/material/List';
+// import Typography from '@mui/material/Typography';
+// import Divider from '@mui/material/Divider';
+// import IconButton from '@mui/material/IconButton';
+// import MenuIcon from '@mui/icons-material/Menu';
+// import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+// import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+// import ListItem from '@mui/material/ListItem';
+// import ListItemButton from '@mui/material/ListItemButton';
+// import ListItemIcon from '@mui/material/ListItemIcon';
+// import ListItemText from '@mui/material/ListItemText';
+// import InboxIcon from '@mui/icons-material/MoveToInbox';
+// import MailIcon from '@mui/icons-material/Mail';
+// import DashboardCustomizeIcon from '@mui/icons-material/DashboardCustomize';
+// import Link from '@mui/material/Link';
+// const drawerWidth = 240;
+
+// const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
+//   ({ theme }) => ({
+//     flexGrow: 1,
+//     padding: theme.spacing(3),
+//     transition: theme.transitions.create('margin', {
+//       easing: theme.transitions.easing.sharp,
+//       duration: theme.transitions.duration.leavingScreen,
+//     }),
+//     marginLeft: `-${drawerWidth}px`,
+//     variants: [
+//       {
+//         props: ({ open }) => open,
+//         style: {
+//           transition: theme.transitions.create('margin', {
+//             easing: theme.transitions.easing.easeOut,
+//             duration: theme.transitions.duration.enteringScreen,
+//           }),
+//           marginLeft: 0,
+//         },
+//       },
+//     ],
+//   }),
+// );
+
+// const AppBar = styled(MuiAppBar, {
+//   shouldForwardProp: (prop) => prop !== 'open',
+// })(({ theme }) => ({
+//   transition: theme.transitions.create(['margin', 'width'], {
+//     easing: theme.transitions.easing.sharp,
+//     duration: theme.transitions.duration.leavingScreen,
+//   }),
+//   variants: [
+//     {
+//       props: ({ open }) => open,
+//       style: {
+//         width: `calc(100% - ${drawerWidth}px)`,
+//         marginLeft: `${drawerWidth}px`,
+//         transition: theme.transitions.create(['margin', 'width'], {
+//           easing: theme.transitions.easing.easeOut,
+//           duration: theme.transitions.duration.enteringScreen,
+//         }),
+//       },
+//     },
+//   ],
+// }));
+
+// const DrawerHeader = styled('div')(({ theme }) => ({
+//   display: 'flex',
+//   alignItems: 'center',
+//   padding: theme.spacing(0, 1),
+//   // necessary for content to be below app bar
+//   ...theme.mixins.toolbar,
+//   justifyContent: 'flex-end',
+// }));
+
+// export default function Sidebar() {
+//   const theme = useTheme();
+//   const [open, setOpen] = React.useState(false);
+
+//   const handleDrawerOpen = () => {
+//     setOpen(true);
+//   };
+
+//   const handleDrawerClose = () => {
+//     setOpen(false);
+//   };
+
+//   return (
+//     <Box sx={{ display: 'flex' }}>
+//       <CssBaseline />
+//       <AppBar position="fixed" open={open}>
+//         <Toolbar>
+//           <IconButton
+//             color="inherit"
+//             aria-label="open drawer"
+//             onClick={handleDrawerOpen}
+//             edge="start"
+//             sx={[
+//               {
+//                 mr: 2,
+//               },
+//               open && { display: 'none' },
+//             ]}
+//           >
+//             <MenuIcon />
+//           </IconButton>
+//           <Typography variant="h6" noWrap component="div">
+//             {/* Persistent drawer */}
+//             ADMIN
+//           </Typography>
+//         </Toolbar>
+//       </AppBar>
+//       <Drawer
+//         sx={{
+//           width: drawerWidth,
+//           flexShrink: 0,
+//           '& .MuiDrawer-paper': {
+//             width: drawerWidth,
+//             boxSizing: 'border-box',
+//           },
+//         }}
+//         variant="persistent"
+//         anchor="left"
+//         open={open}
+//       >
+//         <DrawerHeader>
+//           <IconButton onClick={handleDrawerClose}>
+//             {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+//           </IconButton>
+//         </DrawerHeader>
+//         <Divider />
+//         <List>
+//           {/* {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => ( */}
+//             <ListItem disablePadding>
+//               <ListItemButton component={Link} to={"/Admin/AHome"}>
+//                 <ListItemIcon>
+//                   {/* {index % 2 === 0 ? <InboxIcon /> : <MailIcon />} */}
+//                   <DashboardCustomizeIcon/>
+//                 </ListItemIcon>
+//                 <ListItemText primary="DASHBOARD"/>
+//               </ListItemButton>
+//             </ListItem>
+//             <ListItem disablePadding>
+//               <ListItemButton component={Link} to={"/Admin/Viewuser"}>
+//                 <ListItemIcon>
+//                   {/* {index % 2 === 0 ? <InboxIcon /> : <MailIcon />} */}
+//                   <DashboardCustomizeIcon/>
+//                 </ListItemIcon>
+//                 <ListItemText primary="VIEW USER"/>
+//               </ListItemButton>
+//             </ListItem>
+//           {/* ))} */}
+//         </List>
+//         <Divider />
+
+//       </Drawer>
+//       <Main open={open}>
+//         <DrawerHeader />
+//       </Main>
+//     </Box>
+//   );
+// }
+
 import * as React from 'react';
 import { styled, useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
@@ -18,8 +185,11 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
-import DashboardCustomizeIcon from '@mui/icons-material/DashboardCustomize';
-import Link from '@mui/material/Link';
+
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import { Link } from 'react-router-dom';
+import ViewProduct from './ViewProduct';
+
 const drawerWidth = 240;
 
 const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
@@ -77,6 +247,7 @@ const DrawerHeader = styled('div')(({ theme }) => ({
   justifyContent: 'flex-end',
 }));
 
+
 export default function Sidebar() {
   const theme = useTheme();
   const [open, setOpen] = React.useState(false);
@@ -109,8 +280,7 @@ export default function Sidebar() {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" noWrap component="div">
-            {/* Persistent drawer */}
-            ADMIN
+          Admin
           </Typography>
         </Toolbar>
       </AppBar>
@@ -134,32 +304,61 @@ export default function Sidebar() {
         </DrawerHeader>
         <Divider />
         <List>
-          {/* {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => ( */}
-            <ListItem disablePadding>
-              <ListItemButton component={Link} to={"/Admin/AHome"}>
+            <ListItem  disablePadding>
+              <ListItemButton component={Link} to={"/Admin/AHome"}> 
                 <ListItemIcon>
-                  {/* {index % 2 === 0 ? <InboxIcon /> : <MailIcon />} */}
-                  <DashboardCustomizeIcon/>
+                  <DashboardIcon />
                 </ListItemIcon>
-                <ListItemText primary="DASHBOARD"/>
-              </ListItemButton>
+                <ListItemText primary="DASHBOARD" />
+              </ListItemButton> 
             </ListItem>
-            <ListItem disablePadding>
-              <ListItemButton component={Link} to={"/Admin/Viewuser"}>
-                <ListItemIcon>
-                  {/* {index % 2 === 0 ? <InboxIcon /> : <MailIcon />} */}
-                  <DashboardCustomizeIcon/>
-                </ListItemIcon>
-                <ListItemText primary="VIEW USER"/>
-              </ListItemButton>
-            </ListItem>
-          {/* ))} */}
         </List>
         <Divider />
-
+        <List>
+          {[{label:'Users',link:'/Admin/ManageUser'},{label:'Category',link:'/Admin/ManageCategory'}, {label:'Products',link:'/Admin/ManageProduct'}, {label:'View User', link:'/Admin/ViewUser'}, {label:'View Product', link:'/Admin/ViewProduct'}, {label:'View Category', link:'/Admin/ViewCategory'}, {label:'Add Category', link:'/Admin/AddCategory'}].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+             
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <Link to={text.link} style={{textDecoration:"none"}}>
+                <ListItemText primary={text.label} />
+                   </Link>
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
       </Drawer>
       <Main open={open}>
         <DrawerHeader />
+        {/* <Typography sx={{ marginBottom: 2 }}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
+          enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
+          imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
+          Convallis convallis tellus id interdum velit laoreet id donec ultrices.
+          Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
+          nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
+          leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
+          feugiat vivamus at augue. At augue eget arcu dictum varius duis at
+          consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
+          sapien faucibus et molestie ac.
+        </Typography>
+        <Typography sx={{ marginBottom: 2 }}>
+          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
+          eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
+          neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
+          tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
+          sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
+          tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
+          gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
+          et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
+          tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
+          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
+          posuere sollicitudin aliquam ultrices sagittis orci a.
+        </Typography> */}
       </Main>
     </Box>
   );
